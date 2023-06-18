0902c9a7bf6b3bc245fb1b33c308f280493c4ab2
Nikita Nazarov <nnazarov@ozon.ru>
2023-06-18 07:07:26 +0600

    rebuild bundle

--- .output/server/chunks/app/_nuxt/index-styles.c5f0d97a.mjs
+++
@@ -1,6 +0,0 @@
-const index_vue_vue_type_style_index_0_scoped_73e7ae40_lang = ".root[data-v-73e7ae40]{background-color:#202124;color:#bdc1c6;height:100vh;overflow:hidden;padding:10px;width:100vw}.calcs-container[data-v-73e7ae40]{display:flex;gap:50px;height:100%;overflow:auto}";
-
-const indexStyles_c5f0d97a = [index_vue_vue_type_style_index_0_scoped_73e7ae40_lang];
-
-export { indexStyles_c5f0d97a as default };
-//# sourceMappingURL=index-styles.c5f0d97a.mjs.map
